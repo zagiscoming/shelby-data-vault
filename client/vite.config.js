7f47a97ef2e383ccee2c90498c77c7e75488ef5b
zagiscoming <hakancalsr@gmail.com>
2026-03-13 22:19:38 +0300

    chore: Prepare for GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      // Whether to polyfill `node:` protocol imports.
-      protocolImports: true,
-    }),
-  ],
+  base: '/shelby-data-vault/',
+  plugins: [react()],
 })
